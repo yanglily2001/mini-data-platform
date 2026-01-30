2c94f857a97456dc9287cd4e7062486e80b68afe
Lily Yang <65832486+yanglily2001@users.noreply.github.com>
2026-01-30 11:20:21 -0500

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from .views import health_check
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/health/", health_check),
 ]
