b28e8aa42893304bbee66debfa8b03c274b199bc
Lily Yang <65832486+yanglily2001@users.noreply.github.com>
2026-01-30 13:07:12 -0500

    Update views.py

--- backend/metrics/views.py
+++ backend/metrics/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import csv
+import io
+from datetime import datetime
 
-# Create your views here.
+from django.db import transaction
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
+from .models import Measurement, StagingMeasurement
