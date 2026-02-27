f62f5d6468f814bff74a39c8eb55b470d3627e8f
Lily Yang <65832486+yanglily2001@users.noreply.github.com>
2026-02-27 22:16:07 +0000

    added requirements.txt and urls.py edits

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     path("api/stations/", stations),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/", include("metrics.urls")),
+    # path("api/", include("metrics.urls")),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc")
 ]
 
